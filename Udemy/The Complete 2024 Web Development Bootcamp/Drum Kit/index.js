4fcaff6b8c580882e67849911ad7001b14236d3f
sangeetha875 <sangeethatirunagari875@gmail.com>
2024-05-18 13:18:27 +0530

    Added Sound Management

--- Udemy/The Complete 2024 Web Development Bootcamp/Drum Kit/index.js
+++ Udemy/The Complete 2024 Web Development Bootcamp/Drum Kit/index.js
@@ -1,9 +1,47 @@
-// alert("Hello, Welcome to the Drum Kit Page!!!");
-
 var n = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < n; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("Click "+i);
+    var ch = this.innerHTML;
+    // console.log(ch);
+    switch (ch) {
+      case "w":
+        var tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "l":
+        var bass = new Audio("./sounds/kick-bass.mp3");
+        bass.play();
+        break;
+
+      default:
+        break;
+    }
   });
 }
