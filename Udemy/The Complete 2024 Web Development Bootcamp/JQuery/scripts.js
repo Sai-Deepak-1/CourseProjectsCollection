963b4b1930558863e6fd2d6e61e742529cf9506f
Sai Deepak <psaideepak1@gmail.com>
2024-10-06 14:46:52 +0530

    Add input field and keypress event listener in jQuery

--- Udemy/The Complete 2024 Web Development Bootcamp/JQuery/scripts.js
+++ Udemy/The Complete 2024 Web Development Bootcamp/JQuery/scripts.js
@@ -2,6 +2,14 @@
 
 // $("h1").css("color", "red");
 
-$("document").ready(function() {
-    $("h1").css("color", "red");
-});
+// $("document").ready(function() {
+//     $("h1").css("color", "red");
+// });
+
+// $("h1").click(function () {
+//    $("h1").css("font-family", "Indie Flower"); 
+// });
+
+$(document).keypress(function (event) {
+    $("h1").text($("h1").text()+event.key);
+})
